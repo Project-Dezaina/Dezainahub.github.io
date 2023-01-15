ba3f1f1e7ca86d7c4c3fc9ade13f4684784c4815
mineshpatel029 <mineshptel029@gmail.com>
2023-01-15 18:39:18 +0530

    Updated Navbar

--- userclient/src/components/pages/navbar/Navbar.jsx
+++ userclient/src/components/pages/navbar/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-
+import React from 'react';
+import "./Navbar.css";
+import NavbarImg from './Image/NavLogo.png';
 const Navbar = () => {
   return (
-    <div>
+    <div className='NavigationBarMainContainer'>
+    <div className="NavigationBarImage">
+<img src={NavbarImg} alt="" />
+    </div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    {/* <a class="navbar-brand" href="#">Navbar</a> */}
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse mx-auto" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
