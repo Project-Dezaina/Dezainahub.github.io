56e489cd3e7384cb532e1b19bb5a0410c265409f
DivyankitSingh <divyankithub@gmail.com>
2023-01-17 12:32:18 +0530

    Navbar Css Done

--- userclient/src/components/pages/homePage/HomePageComponents/LandingSection/LandingPage.jsx
+++ userclient/src/components/pages/homePage/HomePageComponents/LandingSection/LandingPage.jsx
@@ -1,31 +1,26 @@
-import React from 'react'
-import "./LandingPage.css"
+import React from "react";
+import "./LandingPage.css";
 import LandingPageImg from "./Image/LandingPageImage.png";
-
 
 const LandingPage = () => {
   return (
-    <div className='LandingPageMainComponents'>
+    <div className="LandingPageMainComponents">
       <div className="landingPageContainer">
         <div className="LandingPageImageSec">
-             {/* <img src={LandingPageImg}  alt="" />  */}
-           {/* <h1 >Hey</h1> */}
-           
-      
-   </div>
-            {<div className ='landing-card'>
-        
-
-        <div class="landing-card1">
-            <div class="card-head"><h1>This is card Head</h1></div>
-            <div class="card-body">This is card body This is card body
-                This is card body
-                This is card body
-                This is card body
-                This is card body
-            </div>
+          {/* <img src={LandingPageImg}  alt="" />  */}
+          {/* <h1 >Hey</h1> */}
         </div>
-        <div class="landing-card2">
+        <div className="landing-card">
+          <div className="landing-card1">
+            <div class="card-head">
+              <h1>This is card Head</h1>
+            </div>
+            <div class="card-body">
+              This is card body This is card body This is card body This is card
+              body This is card body This is card body
+            </div>
+          </div>
+          <div className="landing-card1">
             <div class="card-head"><h1>This is card Head</h1></div>
             <div class="card-body">This is card body This is card body
                 This is card body
@@ -35,7 +30,7 @@ const LandingPage = () => {
                 This is card body
             </div>
         </div>
-        <div class="landing-card3">
+        <div className="landing-card1">
             <div class="card-head"><h1>This is card Head</h1></div>
             <div class="card-body">This is card body This is card body
                 This is card body
@@ -45,19 +40,13 @@ const LandingPage = () => {
                 This is card body
             </div>
         </div>
-      </div> }
-
-           
-          
-    
         </div>
-        <div className="LandingPageContent">
-
-        </div>
+        
       </div>
+      <div className="LandingPageContent"></div>
+    </div>
     // </div>
-  )
-  
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
