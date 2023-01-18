134ec62fa6d41158d9e1e19109e9cc6b261f3017
DivyankitSingh <divyankithub@gmail.com>
2023-01-18 13:39:36 +0530

    Landing page almost done

--- userclient/src/components/pages/homePage/HomePageComponents/LandingSection/LandingPage.jsx
+++ userclient/src/components/pages/homePage/HomePageComponents/LandingSection/LandingPage.jsx
@@ -1,49 +1,38 @@
 import React from "react";
 import "./LandingPage.css";
-import Icon1 from "./Image/Icon1.png";
-import Icon2 from "./Image/Icon2.png";
-import Icon3 from "./Image/Icon3.png";
+import brandImg from "./Image/Landing-img.jpg";
+// import Icon2 from "./Image/Icon2.png";
+// import Icon3 from "./Image/Icon3.png";
 const LandingPage = () => {
   return (
     <div className="LandingPageMainComponents">
-      <div className="landingPageContainer">
-        <div className="LandingPageImageSec"> 
+      <div className="landingPageContainer container">
+        <div className="Landing-Content">
+          <h1>DESIGNING VISUAL STORIES</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+          <div className="landing-buttons">
+            <button type="button" className="discuss-button"><span>Let's Discuss</span></button>
+            <button class="cssbuttons-io-button"> Explore More
+  <div class="icon">
+    <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+  </div>
+</button>
+          </div>
         </div>
-        <div className="landing-card container">
-          <div className="landing-card1">
-            <div className="card-Icon"><img src={Icon1} /></div>
-            <div class="card-head">
-              <h2>CORPORATE BRANDING</h2>
-            </div>
-            <div class="card-body">
-              This is card body This is card body This is card body This is card
-              body This is card body This is card body
-            </div>
+        <div className="Landing-img-box">
+          <div className="landing-box">
+
           </div>
-          <div className="landing-card1">
-            <div className="card-Icon"><img src={Icon2} /></div>
-            <div class="card-head">
-              <h2>ADVERTISING CONTENT</h2>
-            </div>
-            <div class="card-body">
-              This is card body This is card body This is card body This is card
-              body This is card body This is card body This is card body
-            </div>
-          </div>
-          <div className="landing-card1">
-            <div className="card-Icon"><img src={Icon3} /></div>
-            <div class="card-head">
-              <h2>DESIGN RENDERING</h2>
-            </div>
-            <div class="card-body">
-              This is card body This is card body This is card body This is card
-              body This is card bodyThis is card body This is card body This is
-              card body
-            </div>
+          <div className="landing-img">
+              <img src={brandImg} alt="Brand img"/>
           </div>
         </div>
       </div>
-      <div className="LandingPageContent"></div>
+      {/* <div className="LandingPageContent"></div> */}
     </div>
     // </div>
   );
