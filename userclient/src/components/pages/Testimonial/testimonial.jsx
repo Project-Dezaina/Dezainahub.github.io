04c621ac9be147bd52c5f9a14c9e68100ae10eb6
DivyankitSingh <divyankithub@gmail.com>
2023-01-19 19:44:14 +0530

    Made phn responsive to some sections and also completed the landing page and footer

--- userclient/src/components/pages/Testimonial/testimonial.jsx
+++ userclient/src/components/pages/Testimonial/testimonial.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import "./Testimonial.css";
 
-const testimonial = () => {
+const Testimonial = () => {
   return (
-    <div>
-      <div>What Our Clients Say About Us</div>
+    <div className='Testimonial-Main container'>
+    <div className='Testimonial-heading'>
+      <div className='Testimonial-Topic'>Testimonial</div>
+      <div className='Testimonial-head'>What Our <span>Clients</span> Say About Us</div>
+      </div>
       <div>
     <div className='Client-1'>
         <div>Image</div>
@@ -12,23 +16,10 @@ const testimonial = () => {
         <div>Tareef</div>
     </div>
     
-    <div className='Client-1'>
-        <div>Image</div>
-        <div>Client Name</div>
-        <div>Star</div>
-        <div>Tareef</div>
-    </div>
-
     
-    <div className='Client-1'>
-        <div>Image</div>
-        <div>Client Name</div>
-        <div>Star</div>
-        <div>Tareef</div>
-    </div>
       </div>
     </div>
   )
 }
 
-export default testimonial
+export default Testimonial
--- userclient/src/components/pages/homePage/HomePageComponents/LandingSection/LandingPage.jsx
+++ userclient/src/components/pages/homePage/HomePageComponents/LandingSection/LandingPage.jsx
@@ -8,27 +8,81 @@ const LandingPage = () => {
     <div className="LandingPageMainComponents">
       <div className="landingPageContainer container">
         <div className="Landing-Content">
-          <h1>DESIGNING VISUAL STORIES</h1>
+          <h1>DESIGNING <span>VISUAL</span> STORIES</h1>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s
           </p>
           <div className="landing-buttons">
-            <button type="button" className="discuss-button"><span>Let's Discuss</span></button>
-            <button class="cssbuttons-io-button"> Explore More
-  <div class="icon">
-    <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
-  </div>
-</button>
+            <button type="button" className="discuss-button">
+              <span>Let's Discuss</span>
+            </button>
+            <button class="cssbuttons-io-button">
+              {" "}
+              Explore More
+              <div class="icon">
+                <svg
+                  height="24"
+                  width="24"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M0 0h24v24H0z" fill="none"></path>
+                  <path
+                    d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+            </button>
           </div>
         </div>
         <div className="Landing-img-box">
-          <div className="landing-box">
-
-          </div>
+          <div className="landing-box"></div>
           <div className="landing-img">
-              <img src={brandImg} alt="Brand img"/>
+            <img src={brandImg} alt="Brand img" />
+          </div>
+          <div className="landing-small-box">
+            <div className="small-box-content">
+              <div className="SmallBoxContentSvg">
+
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 17 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clip-path="url(#clip0_25_748)">
+                  <path
+                    d="M1.41666 1.41663V13.4583C1.41666 14.6341 2.36583 15.5833 3.54166 15.5833H15.5833"
+                    stroke="#4BACE0"
+                    stroke-width="1.5"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M3.54166 12.0417L6.79291 8.24504C7.33125 7.62171 8.2875 7.57921 8.86833 8.16712L9.54125 8.84004C10.1221 9.42087 11.0783 9.38546 11.6167 8.76212L14.875 4.95837"
+                    stroke="#4BACE0"
+                    stroke-width="1.5"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_25_748">
+                    <rect width="17" height="17" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+              </div>
+              <div className="SmallBocContentP">
+                <p>Lorem Ipsum is simply dummy text</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
