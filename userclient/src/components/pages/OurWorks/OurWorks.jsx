d905e63cbec86dd30f2a3d948b52f547a6031342
mineshpatel029 <mineshptel029@gmail.com>
2023-01-23 02:29:55 +0530

    Updated

--- userclient/src/components/pages/OurWorks/OurWorks.jsx
+++ userclient/src/components/pages/OurWorks/OurWorks.jsx
@@ -1,39 +1,40 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
+
 import "./OurWorks.css"
 import Img1 from "../OurWorks/Image/12.jpeg"
-import Img2 from "../OurWorks/11.jpeg"
+import Img2 from "../OurWorks/Image/11.jpeg"
 import Img3 from "../OurWorks/Image/13.jpeg"
 
 
 const Ourworks = () => {
-  const cards = [
-    {
+  // const cards = [
+  //   {
     
-      fotu: Img1,
-      class:"aaa",
-      // fotu: <img src={Img1} alt=""></img>,
+  //     fotu: Img1,
+  //     class:"aaa",
+  //     // fotu: <img src={Img1} alt=""></img>,
 
-      title: "card-1",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
-    },
-    {
-        fotu: Img2,
-      class:"bbb",
+  //     title: "card-1",
+  //     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
+  //   },
+  //   {
+  //       fotu: Img2,
+  //     class:"bbb",
 
-      // fotu: <img src={Img2} alt=""></img>,
-        title: "card-2",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
-      },
-      {
-      class:"ccc",
-      fotu: Img3,
-      // fotu: <img src={Img3} alt=""></img>,
-        title: "card-3",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
-      },
+  //     // fotu: <img src={Img2} alt=""></img>,
+  //       title: "card-2",
+  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
+  //     },
+  //     {
+  //     class:"ccc",
+  //     fotu: Img3,
+  //     // fotu: <img src={Img3} alt=""></img>,
+  //       title: "card-3",
+  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
+  //     },
 
-  ];
+  // ];
   return (
     <div className="OurWork-Main">
       <div className="container">
@@ -41,13 +42,40 @@ const Ourworks = () => {
 
         <div className="work-cards">
           <div className="Work-card-body">
-            {cards.map((card, i) => (
+            {/* {cards.map((card, i) => (
               <div key={i} className="card">
                 <div className="Work-Photo"><img src={card.fotu} alt="" /></div>
                 <h3>{card.title}</h3>
                 <p>{card.text}</p>
               </div>
-            ))}
+            ))} */}
+            <div className="New-Card">
+              <div className="card-img">
+                <img src={Img1} alt="" />
+              </div>
+              <div className="NewCard-body">
+                <h3>Project Rendering</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Necessitatibus aut praesentium officia eveniet ad est
+                </p>
+              </div>
+            </div>
+            <div className="New-Card-V2">
+              <div className="card-img">
+                <img src={Img1} alt="" />
+              </div>
+              <div className="NewCard-body">
+              <div className="Body-Header">
+              <h3>Project Rendering</h3>
+              </div>
+                
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Necessitatibus aut praesentium officia eveniet ad est
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
