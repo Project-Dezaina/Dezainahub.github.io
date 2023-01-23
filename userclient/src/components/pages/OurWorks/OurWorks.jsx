bec1da837ad98a757d24536124e4ac55d4270008
mineshpatel029 <mineshptel029@gmail.com>
2023-01-24 01:19:20 +0530

    updated our works

--- userclient/src/components/pages/OurWorks/OurWorks.jsx
+++ userclient/src/components/pages/OurWorks/OurWorks.jsx
@@ -1,55 +1,82 @@
 import React from "react";
 // import { useState } from "react";
 
-import "./OurWorks.css"
-import Img1 from "../OurWorks/Image/12.jpeg"
-import Img2 from "../OurWorks/Image/11.jpeg"
-import Img3 from "../OurWorks/Image/13.jpeg"
-
+import "./OurWorks.css";
+import Img1 from "../OurWorks/Image/12.jpeg";
+import Img2 from "../OurWorks/Image/11.jpeg";
+import Img3 from "../OurWorks/Image/13.jpeg";
 
 const Ourworks = () => {
-  // const cards = [
-  //   {
-    
-  //     fotu: Img1,
-  //     class:"aaa",
-  //     // fotu: <img src={Img1} alt=""></img>,
 
+  // const card = [
+  //   {
+  //        id : "aa",
+  //     fotu: Img1,
+  //     class:"New-Card",
+  //     // fotu: <img src={Img1} alt=""></img>,
   //     title: "card-1",
   //     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
   //   },
   //   {
-  //       fotu: Img2,
-  //     class:"bbb",
+  //        id : "dd",
+  //           class:"New-Card-V2",
+  //             fotu: Img2,
 
   //     // fotu: <img src={Img2} alt=""></img>,
   //       title: "card-2",
   //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
   //     },
   //     {
-  //     class:"ccc",
+  //        id : "cc",
+  //     class:"New-Card-V2",
   //     fotu: Img3,
   //     // fotu: <img src={Img3} alt=""></img>,
   //       title: "card-3",
   //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
   //     },
+  // {
+  //    id : "bb",
+  //   class:"ourWorksBio New-Card-V3",
+  //   // fotu: Img3,
+  //   // fotu: <img src={Img3} alt=""></img>,
+  //     title: "or Portfolio",
+  //     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis possimus tenetur voluptatum iste esse culpa, iure vitae doloremque sit. Maxime.",
+  //   },
 
   // ];
   return (
     <div className="OurWork-Main">
       <div className="container">
-        <h1>Cards</h1>
+        {/* <h1>Cards</h1> */}
 
         <div className="work-cards">
           <div className="Work-card-body">
             {/* {cards.map((card, i) => (
               <div key={i} className="card">
+              <div className={card.class} id={card.id}>
                 <div className="Work-Photo"><img src={card.fotu} alt="" /></div>
                 <h3>{card.title}</h3>
                 <p>{card.text}</p>
               </div>
+              </div>
             ))} */}
-            <div className="New-Card">
+             {/* {card.map((card, i) => (
+             <div key={i} className="card">
+              <div className={card.class} id={card.id}>
+              <div className="card-img">
+                <img src={card.fotu} alt="" />
+              </div>
+              <div className="NewCard-body">
+                <h3>{card.title}</h3>
+                <p>
+                 {card.text}
+                </p>
+              </div>
+            </div>
+              </div>
+            ))}  */}
+            <div className="Work-card-body">
+            <div className="New-Card" id="aa">
               <div className="card-img">
                 <img src={Img1} alt="" />
               </div>
@@ -61,20 +88,53 @@ const Ourworks = () => {
                 </p>
               </div>
             </div>
-            <div className="New-Card-V2">
+            <div className="New-Card " id="bb">
               <div className="card-img">
                 <img src={Img1} alt="" />
               </div>
               <div className="NewCard-body">
-              <div className="Body-Header">
-              <h3>Project Rendering</h3>
-              </div>
-                
+                <div className="Body-Header">
+                  <h3>Project Rendering</h3>
+                </div>
+
                 <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Necessitatibus aut praesentium officia eveniet ad est
                 </p>
               </div>
+            </div>
+
+            <div className="New-Card" id="cc">
+              <div className="card-img">
+                <img src={Img1} alt="" />
+              </div>
+              <div className="NewCard-body">
+                <div className="Body-Header">
+                  <h3>Project Rendering</h3>
+                </div>
+
+                <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Necessitatibus aut praesentium officia eveniet ad est
+                </p>
+              </div>
+            </div>
+            <div className="ourWorksBio New-Card-V3" id="dd">
+            <div className="DD">
+            <h3>See <span>Our Works</span> or Portfolio </h3>
+              {/* <h3>or Portfolio</h3> */}
+              <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Necessitatibus aut praesentium officia eveniet ad est
+                  </p>
+              <div className="OurWorks-button">
+            <button class="button-5" role="button">
+             Explore Gallery
+            </button>
+          </div>
+            </div>
+            
+            </div>
             </div>
           </div>
         </div>
