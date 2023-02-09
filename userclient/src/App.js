d53d25c00b4b96f4df2d8de486f4238325a50325
mineshpatel029 <mineshptel029@gmail.com>
2023-02-09 15:48:19 +0530

    footerUpdated

--- userclient/src/App.js
+++ userclient/src/App.js
@@ -3,22 +3,22 @@ import "./App.css";
 // import Homepage from "./components/pages/homePage/Homepage";
 // import AboutPage from "./components/MainPages/AboutPage/AboutPage";
 // import ProductPage from "./components/MainPages/ProductPage/ProductPage";
-import ProductPage from "./components/MainPages/ProductPage/ProductPage";
+// import ProductPage from "./components/MainPages/ProductPage/ProductPage";
 // import GalleryPage from "./components/MainPages/GalleryPage/GalleryPage";
-// import ContactUSContext from "./components/MainPages/ContactUsPage/ContactUsContext/ContactUSContext";
-// import ContactUsPage from "./components/MainPages/ContactUsPage/ContactUsPage";
+import ContactUSContext from "./components/MainPages/ContactUsPage/ContactUsContext/ContactUSContext";
+import ContactUsPage from "./components/MainPages/ContactUsPage/ContactUsPage";
 function App() {
   return (
     <div className="App">
     
       {/* <Homepage /> */}
       {/* <AboutPage/> */}
-      <ProductPage/>
+      {/* <ProductPage/> */}
       {/* <GalleryPage/> */}
-      {/* <ContactUSContext>
+      <ContactUSContext>
       <ContactUsPage/>
 
-      </ContactUSContext> */}
+      </ContactUSContext>
      {/* <Cstage1/> */}
       
 
